d1bdd8a1e5e4207a5d266093c5c19ed9176b79b4
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2023-02-05 22:27:43 -0600

    Add javascript

---
+++ public/javascript/edit-post.js
@@ -0,0 +1,24 @@
+async function editFormHandler(event) {
+  event.preventDefault();
+  const title = document.querySelector('input[name="post-title"]').value;
+  const post_url = document.querySelector('input[name="post-url"]').value;
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      title,
+      post_url,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document
+  .querySelector('.edit-post-form')
+  .addEventListener('submit', editFormHandler);
---
+++ public/javascript/logout.js
@@ -0,0 +1,15 @@
+// function to logout the user
+const logout = async (event) => {
+  event.preventDefault();
+  const response = await fetch('/api/users/logout', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+  });
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document.querySelector('#logout').addEventListener('click', logout);
